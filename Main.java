6e9c86650809287a9b60139216ad21cea874927b
David L <124907735+nstrings@users.noreply.github.com>
2023-02-28 04:26:15 +0000

    done

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Enter a word:");
+    Scanner ss = new Scanner(System.in);
     
-    //Your code will return the length of the entered String
+    System.out.println("Input any word, and I will tell you how many letters that word has:\n\n");
+    String word = ss.nextLine();
+    int length= word.length();
+    
+    System.out.println("\n\n" + word + " has " + length + " letters! ");
+    
   }
 }
